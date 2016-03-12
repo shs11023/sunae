1685d0086d7631628173d6f842e370917fe07c29
hsseo <shs11023@gmail.com>
2016-03-12 14:49:02 +0900

    add a assigment 20160317

---
+++ HTML5_20160317_Hsseo/scripts/loader.js
@@ -0,0 +1,53 @@
+var jewel = {
+    screens : {},
+	settings : {
+        rows : 8,
+        cols : 8,
+        baseScore : 100,
+        numJewelTypes : 7
+    }
+};
+
+window.addEventListener("load", function() {
+
+Modernizr.addTest("standalone", function() {
+    return (window.navigator.standalone != false);
+});
+
+// loading stage 1
+Modernizr.load([
+{ 
+    load : [
+        "scripts/sizzle.js",
+        "scripts/dom.js",
+        "scripts/game.js"
+    ]
+},{
+    test : Modernizr.standalone,
+    yep : "scripts/screen.splash.js",
+    nope : "scripts/screen.install.js",
+    complete : function() {
+        jewel.game.setup();
+        if (Modernizr.standalone) {
+            jewel.game.showScreen("splash-screen");
+        } else {
+            jewel.game.showScreen("install-screen");
+        }
+    }
+}
+]);
+
+// loading stage 2
+if (Modernizr.standalone) {
+    Modernizr.load([
+    {
+        load : [
+            "scripts/screen.main-menu.js",
+            "scripts/board.js"
+        ]
+    }
+    ]);
+}
+
+
+}, false);
